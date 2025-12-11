a8158635b9c4e3d94775534581f8ca680a116caa
jmunozpe <jmunozpe@unal.edu.co>
2025-12-11 15:30:45 -0500

    Update example_rc_series.py

--- examples/example_rc_series.py
+++ examples/example_rc_series.py
@@ -4,13 +4,14 @@ from circuit_simulator.circuits import RCSeriesCircuit
 from circuit_simulator.plotting import plot_rc_series
 import matplotlib.pyplot as plt
 
-
 source = DCSupply(5.0)
+
 R = Resistor("R1", 1000)
 C = Capacitor("C1", 100e-6)
 
 circuit = RCSeriesCircuit(source, R, C)
-result = circuit.simulate(t_end=0.5, dt=1e-4)
+result = circuit.simulate(t_end=1.0, dt=1e-4)
 
 plot_rc_series(result)
 plt.show()
+
