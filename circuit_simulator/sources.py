c6e7b5cf70282c9eb291cc0841d8c3ed6f6d1d9d
jmunozpe <jmunozpe@unal.edu.co>
2025-12-10 22:11:37 -0500

    Update sources.py

--- circuit_simulator/sources.py
+++ circuit_simulator/sources.py
@@ -1,1 +1,11 @@
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class DCSupply:
+    voltage: float
+
+    def value(self, t: np.ndarray) -> np.ndarray:
+        return self.voltage * np.ones_like(t)
 
