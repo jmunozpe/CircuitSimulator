4f7e8cc6a39898711245ada205c770a1ddd0d522
jmunozpe <jmunozpe@unal.edu.co>
2025-12-11 15:31:19 -0500

    Update RL circuit parameters and simulation duration

--- examples/example_rl_series.py
+++ examples/example_rl_series.py
@@ -4,13 +4,14 @@ from circuit_simulator.circuits import RLSeriesCircuit
 from circuit_simulator.plotting import plot_rl_series
 import matplotlib.pyplot as plt
 
+source = DCSupply(10.0)
 
-source = DCSupply(5.0)
-R = Resistor("R1", 100)
-L = Inductor("L1", 10e-3)
+R = Resistor("R1", 50)
+L = Inductor("L1", 0.2)
 
 circuit = RLSeriesCircuit(source, R, L)
-result = circuit.simulate(t_end=0.1, dt=1e-5)
+result = circuit.simulate(t_end=1.0, dt=1e-4)
 
 plot_rl_series(result)
 plt.show()
+
